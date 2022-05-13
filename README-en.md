7c69a0f0da43ff4eaf6a244c3ffef9b22aa5a44e
ximoqing <ximoqing@miyear.club>
2022-05-13 16:40:11 +0800

    add

---
+++ README-en.md
@@ -0,0 +1,9 @@
+# Magicaltool
+[中文](README.md)|[English](README-en.md)
+
+## Function
+----
+- 快速配置
+- 插件下载
+- 对齐（中间、上下、左右、对角）
+- 开孔
---
+++ __init__.py
@@ -0,0 +1,61 @@
+bl_info = {
+	"name": "Magicaltool",
+	"author": "ximoqing",
+	"version": (1,0),
+	"blender": (2,93,3.0,3.1),
+	"location": "Node Editor > nodes_example",
+	"description": "blender tool",
+	"wiki_url":	 "http://www.tenwei.club",
+	"warning": "",
+	"category": "Node"
+}
+
+import bpy
+from bpy.app.handlers import persistent
+
+@persistent
+def load_handler_for_preferences(_):
+    print("Changing Preference Defaults!")
+    from bpy import context
+
+    prefs = context.preferences
+    prefs.use_preferences_save = False
+
+    kc = context.window_manager.keyconfigs["blender"]
+    kc_prefs = kc.preferences
+    if kc_prefs is not None:
+        kc_prefs.select_mouse = 'RIGHT'
+        kc_prefs.spacebar_action = 'SEARCH'
+        kc_prefs.use_pie_click_drag = True
+
+    view = prefs.view
+    view.header_align = 'BOTTOM'
+
+
+@persistent
+def load_handler_for_startup(_):
+    print("Changing Startup Defaults!")
+
+    # Use smooth faces.
+    for mesh in bpy.data.meshes:
+        for poly in mesh.polygons:
+            poly.use_smooth = True
+
+    # Use material preview shading.
+    for screen in bpy.data.screens:
+        for area in screen.areas:
+            for space in area.spaces:
+                if space.type == 'VIEW_3D':
+                    space.shading.type = 'MATERIAL'
+                    space.shading.use_scene_lights = True
+
+
+def register():
+    print("Registering to Change Defaults")
+    bpy.app.handlers.load_factory_preferences_post.append(load_handler_for_preferences)
+    bpy.app.handlers.load_factory_startup_post.append(load_handler_for_startup)
+
+def unregister():
+    print("Unregistering to Change Defaults")
+    bpy.app.handlers.load_factory_preferences_post.remove(load_handler_for_preferences)
+    bpy.app.handlers.load_factory_startup_post.remove(load_handler_for_startup)
